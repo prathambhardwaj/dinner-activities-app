57d9f0fb7f23c7dc531941d06ff79011001d69b4
pratham <prathambhardwaj81@gmail.com>
2024-09-04 10:15:03 +0530

    final one

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import CardList from "./CardList";
 import "./App.css"; // Add your custom styles
 
 const dinnerItems = [
-  { id: 1, name: "Option 1", description: "Mocha" },
-  { id: 2, name: "Option 2", description: "Bodega" },
-  { id: 3, name: "Option 3", description: "Farzi Cafe" },
-  { id: 4, name: "Option 4", description: "CC" },
-  { id: 5, name: "Option 5", description: "Yacht Club" },
-  { id: 6, name: "Option 6", description: "T9" },
-  { id: 7, name: "Option 7", description: "Dhuaan" },
+  { id: 1, name: "Dinner Option 1", description: "Mocha" },
+  { id: 2, name: "Dinner Option 2", description: "Bodega" },
+  { id: 3, name: "Dinner Option 3", description: "Farzi Cafe" },
+  { id: 4, name: "Dinner Option 4", description: "CC" },
+  { id: 5, name: "Dinner Option 5", description: "Yacht Club" },
+  { id: 6, name: "Dinner Option 6", description: "T9" },
+  { id: 7, name: "Dinner Option 7", description: "Dhuaan" },
   // Add more items as needed
 ];
 
@@ -30,7 +30,7 @@ const dayActivities = [
 
 function App() {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [category, setCategory] = useState("dinner");
+  const [category, setCategory] = useState("day");
 
   const handleCardClick = (item) => {
     setSelectedItem(selectedItem?.id === item.id ? null : item);
