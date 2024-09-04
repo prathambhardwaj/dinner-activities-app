431089941a8ed46e9306a93feaac2edb84a0afa3
pratham <prathambhardwaj81@gmail.com>
2024-09-04 10:03:13 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const dinnerItems = [
   { id: 1, name: "Option 1", description: "Mocha" },
   { id: 2, name: "Option 2", description: "Bodega" },
   { id: 3, name: "Option 3", description: "Farzi Cafe" },
+  { id: 4, name: "Option 4", description: "CC" },
+  { id: 5, name: "Option 5", description: "Yacht Club" },
+  { id: 6, name: "Option 5", description: "T9" },
   // Add more items as needed
 ];
 
@@ -37,8 +40,18 @@ function App() {
   return (
     <div className="App">
       <header>
-        <button onClick={() => handleCategoryChange("dinner")}>Dinner</button>
-        <button onClick={() => handleCategoryChange("day")}>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => handleCategoryChange("dinner")}
+        >
+          Dinner
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => handleCategoryChange("day")}
+        >
           Day Activities
         </button>
       </header>
